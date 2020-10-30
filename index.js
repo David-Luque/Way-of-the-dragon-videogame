9036b41f0a93a9ee6d570b96dc7ec2938ccfdeed
David luque Alba <dvdlq99@gmail.com>
2020-10-30 23:40:45 +0100

    basic data code and logic

--- index.js
+++ index.js
@@ -1,1 +1,105 @@
-function () {}
+
+window.onload = () => {
+    document.getElementById('#start-game').addEventListener('click', ()=>{
+
+        const ctx = document.getElementById('main-canvas').getContext('2d');
+        ctx.drawImage = (dojoImg, 0, 0, canvaswidth, canvaslength);
+        ctx.fillStyle = 'color';
+        ctx.font = 'size | fuente molona oriental';
+
+        //FUNCION PRINCIPAL; LA QUE ITERA DURANTE EL JUEGO
+        const mainFunction = () => {
+            updateData()
+            clearCanvas()
+            drawAll()
+            requestAnimationFrame(mainFunction)
+        }
+        
+        //FUNCIONES SUBPRINCIPALES
+        const updateData = () => {
+            const defeatedEnemies //contador de muertos y puntos: ¿array o contador simple?
+            ctx.fillText(`SCORE ${defeatedEnemies.length}`, posX, posY)
+            
+            updateBruce();
+            updateEnemies();
+        };
+        
+        const clearCanvas = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas. height);
+        };
+
+        const drawAll = () => {
+            ctx.drawImage(bruceLee.image, posX, posY, width, height)
+            //aqui: dibujar todos los enemigos activos en cada momento segun su posicion actual
+        }
+       
+       
+        //FUNCIONES MENORES
+        const updateBruce = () => {}; //FUNCION INUTIL DE MOMENTO. PARA BONUS
+        
+        const updateEnemies = () => {
+            //aqui: actualizar posicion en x y en y para cada uno
+        };
+        
+        
+        //EVENTLISTENERS
+        document.addEventListener('keydown', (touch) => {
+            switch(touch) {
+                case touch.key === 'ArrowUp':
+                    bruceLee.direction = 'up';
+                    bruceLee.image = bruceImgUp;
+                    break;
+                case touch.key === 'ArrowDown':
+                    bruceLee.direction = 'down';
+                    bruceLee.image = bruceImgUp
+                    break;
+                case touch.key === 'ArrowLeft':
+                    bruceLee.direction = 'left';
+                    bruceLee.image = bruceImgLeft;
+                    break;
+                case touch.key === 'ArrowRight':
+                    bruceLee.direction = 'right';
+                    bruceLee.image = bruceImgRight;
+                    break;
+                
+            }
+        })
+
+
+        //FUNCION PARA CHECKEAR LA CARGA DE IMAGENES
+       const checkLoadComplete = () => {
+            const checkArray = [];
+            const allImages = [
+                dojoImg,
+                fighterImgLeft,
+                fighterImgRight,
+                bruceImgUp,
+                bruceImgDowm,
+                bruceImgLeft,
+                bruceImgRight
+            ];
+            
+            allImages.forEach(image => {
+                image.onload = () => {
+                    checkArray.push(true);
+                }
+                if(checkArray.length === allImages.length){
+                    mainFunction()
+                }
+            })
+       }
+       
+       
+       //CALL FUNCTION AND START OF EVERYTHING
+       checkLoadComplete();
+
+
+
+
+        
+
+        
+    })
+}
+
+
--- memory.js
+++ memory.js
@@ -1,6 +1,88 @@
-class Example {
-    constructor() {
-        this.Example = y;
-        this.atribute = x;
+
+//TODAS LAS IMAGENES
+const dojoImg = new Image();
+dojoImg.src = ''
+const fighterImgLeft = new Image();
+fighterImgLeft.src = ''
+const fighterImgRight = new Image();
+fighterImgRight.src = ''
+const bruceImgLeft = new Image();
+bruceImgLeft.src = ''
+const bruceImgRight= new Image();
+bruceImgRight.src = ''
+const bruceImgDowm = new Image();
+bruceImgDowm.src = ''
+const bruceImgUp = new Image();
+bruceImgUp.src = ''
+//ARRIBA FALTAN LAS URL's
+
+
+//¿HACER CLASS PARA GAME-AREA?
+
+class Fighter {
+    constructor(_positionX, _positionY, _direction) {
+        this.positionX = _positionX;
+        this.positionY = _positionY;
+        this.direction = _direction;
+        this.image = imagen segun posicion inicial;
+        // this.imageLeft = fighterImgLeft;
+        // this.imageRight = fighterImgRight;
+    }
+    moveLeft() {
+        this.positionX -= 1;
+    }
+    moveRight() {
+        this.positionX += 1;
+    }
+    moveUp() {
+        this.positionY -= 1;
+    }
+    moveDown() {
+        this.positionY += 1;
     }
 }
+
+
+
+const bruceLee = {
+    health: 3,
+    image: bruceImgLeft,
+    //imageLeft: bruceImgLeft;
+    //imageRight: bruceImgRight;
+    //imageUp: bruceImgUp;
+    //imageDown: bruceImgDowm;
+
+    changeDirection: () => {},
+    
+    sufferDamage: () => {
+        health--
+    },
+
+    crashWith: () => {
+        //para choque con enemigos
+    }
+}
+
+
+
+// class MasterFighter extends Fighter {
+//     constructor (_direction, _health) {
+//         super(_direction)
+//         this.health = _health;
+//         this.image = bruceImgLeft;
+//         // this.imageLeft = bruceImgLeft;
+//         // this.imageRight = bruceImgRight;
+//         // this.imageUp = bruceImgUp;
+//         // this.imageDown = bruceImgDowm;
+//     }
+    
+//     changeDirection() {}
+    
+//     sufferDamage() {
+//         this.health--;
+//     };
+
+//     crashWith() {
+//         //para choque con enemigos
+//     }  
+// }
