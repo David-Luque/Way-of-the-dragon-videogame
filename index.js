0720315193685c11e26396b6754ff5f8cdf5c273
David luque Alba <dvdlq99@gmail.com>
2020-10-31 23:20:25 +0100

    added some visual features and logic

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 
 window.onload = () => {
-    document.getElementById('#start-game').addEventListener('click', ()=>{
+    document.getElementById('start-game').addEventListener('click', ()=>{
+        const canvas = document.getElementById('main-canvas');
+        const ctx = canvas.getContext('2d');
+        const viewport = {
+            width: parseInt(canvas.getAttribute("width")),
+            height: parseInt(canvas.getAttribute("height"))
+        }
+        const game = new GameArea;
+        ctx.font = '30px Arial';
+        ctx.fillStyle = "white";
 
-        const ctx = document.getElementById('main-canvas').getContext('2d');
-        ctx.drawImage = (dojoImg, 0, 0, canvaswidth, canvaslength);
-        ctx.fillStyle = 'color';
-        ctx.font = 'size | fuente molona oriental';
+        
+        //DISPLAY DEL SETUP
+        ctx.fillRect((viewport.width / 2) - 25, (viewport.height / 2) - 25, 50, 50);
+        ctx.fillText(`SCORE: ${game.score}`, 350, 30);
+        ctx.fillText(`HEALTH: ${game.health}`, 55, 30);
 
+        const fighter1 = new Fighter(0, 240, DIRECTION.LEFT);
+        console.log(fighter1);
+
+        // //FUNCION GENERADORA DE ENEMIGOS, TANTO IMAGEN COMO DATA
+        // const enemiesGenerator = ()=>{}
+        
+        
         //FUNCION PRINCIPAL; LA QUE ITERA DURANTE EL JUEGO
         const mainFunction = () => {
             updateData()
             clearCanvas()
             drawAll()
             requestAnimationFrame(mainFunction)
-        }
+        };
         
         //FUNCIONES SUBPRINCIPALES
         const updateData = () => {
-            const defeatedEnemies //contador de muertos y puntos: ¿array o contador simple?
-            ctx.fillText(`SCORE ${defeatedEnemies.length}`, posX, posY)
-            
-            updateBruce();
             updateEnemies();
+            checkColision()
+            endGameCheck();
         };
         
         const clearCanvas = () => {
@@ -29,69 +44,128 @@ window.onload = () => {
         };
 
         const drawAll = () => {
-            ctx.drawImage(bruceLee.image, posX, posY, width, height)
-            //aqui: dibujar todos los enemigos activos en cada momento segun su posicion actual
+            ctx.fillText(`SCORE: ${game.score}`, 350, 30);
+            ctx.fillText(`HEALTH: ${game.health}`, 55, 30);
+            drawBruce();
+            drawEnemies();
         }
        
        
         //FUNCIONES MENORES
-        const updateBruce = () => {}; //FUNCION INUTIL DE MOMENTO. PARA BONUS
-        
+
         const updateEnemies = () => {
-            //aqui: actualizar posicion en x y en y para cada uno
+            //aqui: actualizar posicion de enemigos por cada uno
+            fighter1.moveRight();
+        };
+
+        const checkColision = () => {};
+        
+        
+        const endGameCheck = ()=>{
+            if(game.health === 0) {
+                isGameOver()
+            }
+        } 
+        
+        
+        const drawBruce = () => {
+            // /*
+            //  * TODO: Understand why I can do this.
+            //  * bruceImg[bruceLee.direction] -> bruceImg.down -> bruceImageDown
+            //  * When you don't know the value of the object property, but it saved in a variable
+            //  * you use brackets("[]").
+            //  */
+            // const bruceImage = bruceImg[bruceLee.direction];
+            // ctx.drawImage(bruceImage, viewport.width / 2, viewport.height / 2, 50, 50);
+            ctx.fillRect((viewport.width / 2) - 25, (viewport.height / 2) - 25, 50, 50);
+        };
+        3
+        const drawEnemies = () => {
+            //empezar con 1 y luego loop
+            ctx.drawImage(fighterImgLeft, fighter1.positionX, fighter1.positionY, 50, 50)
         };
         
-        
-        //EVENTLISTENERS
-        document.addEventListener('keydown', (touch) => {
-            switch(touch) {
-                case touch.key === 'ArrowUp':
-                    bruceLee.direction = 'up';
-                    bruceLee.image = bruceImgUp;
-                    break;
-                case touch.key === 'ArrowDown':
-                    bruceLee.direction = 'down';
-                    bruceLee.image = bruceImgUp
-                    break;
-                case touch.key === 'ArrowLeft':
-                    bruceLee.direction = 'left';
-                    bruceLee.image = bruceImgLeft;
-                    break;
-                case touch.key === 'ArrowRight':
-                    bruceLee.direction = 'right';
-                    bruceLee.image = bruceImgRight;
-                    break;
+
+        // //EVENTLISTENERS FINAL
+        // document.addEventListener('keydown', (touch) => {
+        //     switch(touch) {
+        //         case 'ArrowUp':
+        //             bruceLee.changeDirection(DIRECTION.UP)
+        //             break;
+        //         case 'ArrowDown':
+        //             bruceLee.direction = 'down';
+        //             bruceLee.image = bruceImgUp
+        //             break;
+        //         case 'ArrowLeft':
+        //             bruceLee.direction = 'left';
+        //             bruceLee.image = bruceImgLeft;
+        //             break;
+        //         case 'ArrowRight':
+        //             bruceLee.direction = 'right';
+        //             bruceLee.image = bruceImgRight;
+        //             break;
                 
+        //     }
+        // })
+
+        
+        mainFunction()
+        
+       
+        //EVENTLISTENERS temporal
+        document.addEventListener('keydown', (touch) => {
+            switch(touch.key) {
+            case 'ArrowUp':
+                ctx.fillStyle = 'green';
+                ctx.fillRect((viewport.width / 2) - 25, (viewport.height / 2) - 25, 50, 50);
+                console.log(touch);
+                break;
+            case 'ArrowDown':
+                ctx.fillStyle = 'red';
+                ctx.fillRect((viewport.width / 2) - 25, (viewport.height / 2) - 25, 50, 50);;
+                console.log(touch);
+                break;
+            case 'ArrowLeft':
+                ctx.fillStyle = 'blue';
+                ctx.fillRect((viewport.width / 2) - 25, (viewport.height / 2) - 25, 50, 50);;
+                console.log(touch);
+                break;
+            case 'ArrowRight':
+                ctx.fillStyle = 'yellow';
+                ctx.fillRect((viewport.width / 2) - 25, (viewport.height / 2) - 25, 50, 50);;
+                console.log(touch);
+                break;
             }
         })
-
-
+       
+       
         //FUNCION PARA CHECKEAR LA CARGA DE IMAGENES
-       const checkLoadComplete = () => {
-            const checkArray = [];
-            const allImages = [
-                dojoImg,
-                fighterImgLeft,
-                fighterImgRight,
-                bruceImgUp,
-                bruceImgDowm,
-                bruceImgLeft,
-                bruceImgRight
-            ];
+    //     const checkLoadComplete = () => {
+    //         const checkArray = [];
+    //         const allImages = [
+    //             tatamiImg,
+    //             fighterImgLeft,
+    //             fighterImgRight,
+    //             bruceImgUp,
+    //             bruceImgDowm,
+    //             bruceImgLeft,
+    //             bruceImgRight
+    //         ];
             
-            allImages.forEach(image => {
-                image.onload = () => {
-                    checkArray.push(true);
-                }
-                if(checkArray.length === allImages.length){
-                    mainFunction()
-                }
-            })
-       }
+    //         allImages.forEach(image => {
+    //             image.onload = () => {
+    //                 checkArray.push(true);
+                    
+    //                 if(checkArray.length === allImages.length){
+    //                     mainFunction()
+    //                 }
+    //             }
+    //         })
+    //    }
        
        
        //CALL FUNCTION AND START OF EVERYTHING
-       checkLoadComplete();
+       //checkLoadComplete();
 
 
 
--- memory.js
+++ memory.js
@@ -1,9 +1,18 @@
+// See: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Modo_estricto
+'use strict';
+
+const canvas = document.getElementById('main-canvas');
+const ctx = canvas.getContext('2d');
+const viewport = {
+    width: parseInt(canvas.getAttribute("width")),
+    height: parseInt(canvas.getAttribute("height"))
+}
 
 //TODAS LAS IMAGENES
-const dojoImg = new Image();
-dojoImg.src = ''
+const tatamiImg = new Image();
+tatamiImg.src = './images/tatami.jpg';
 const fighterImgLeft = new Image();
-fighterImgLeft.src = ''
+fighterImgLeft.src = './images/enemies.jpg'
 const fighterImgRight = new Image();
 fighterImgRight.src = ''
 const bruceImgLeft = new Image();
@@ -16,23 +25,85 @@ const bruceImgUp = new Image();
 bruceImgUp.src = ''
 //ARRIBA FALTAN LAS URL's
 
+const DIRECTION = {
+    DOWN: 'down',
+    RIGHT: 'right',
+    LEFT: 'left',
+    UP: 'up',
+}
 
-//¿HACER CLASS PARA GAME-AREA?
+const bruceImg = {
+    [DIRECTION.UP]: bruceImgUp,
+    [DIRECTION.DOWN]: bruceImgDowm,
+    [DIRECTION.LEFT]: bruceImgLeft,
+    [DIRECTION.RIGHT]: bruceImgRight
+};
+
+const FIGHTER_HEIGHT = 100;
+
+
+class GameArea {
+    constructor(){
+        this.health = 3;
+        this.enemies = [];
+        this.score = 0;
+    }
+    start(){
+        this.setInterval(()=>{
+            this.enemiesGeneration
+        }, 1000)
+    }
+    isGameOver() {
+        if(health === 0) {
+            clearInterval(this.setInterval);
+
+        }
+    }
+    createEnemy(){
+        //this.enemies.push()
+    }
+    //gameStop(){}
+
+    //gamerestart(){}
+
+}
+
+const game = new GameArea;
+
+//DEFINIR INSTANCIA CON LA POSICION Y ORIENTACION
+//const fighterTop = new Fighter(width / 2 - figherWith / 2, 0, DIRECTION.DOWN)
 
 class Fighter {
     constructor(_positionX, _positionY, _direction) {
         this.positionX = _positionX;
         this.positionY = _positionY;
         this.direction = _direction;
-        this.image = imagen segun posicion inicial;
+        // this.image = imagen segun posicion inicial;
         // this.imageLeft = fighterImgLeft;
         // this.imageRight = fighterImgRight;
+        switch(_direction){
+            case DIRECTION.DOWN:
+                this.move = this.moveDown;
+            break;
+            case DIRECTION.RIGHT:
+                this.move = this.moveRight;
+            break;
+            case DIRECTION.LEFT:
+                this.move = this.moveLeft;
+            break;
+            case DIRECTION.UP:
+                this.move = this.moveUp;
+            break;
+        }
     }
     moveLeft() {
         this.positionX -= 1;
     }
     moveRight() {
         this.positionX += 1;
+        if(200 < this.positionX && this.positionX > 210) {
+            this.positionX = 512;
+        }
     }
     moveUp() {
         this.positionY -= 1;
@@ -45,44 +116,17 @@ class Fighter {
 
 
 const bruceLee = {
-    health: 3,
-    image: bruceImgLeft,
-    //imageLeft: bruceImgLeft;
-    //imageRight: bruceImgRight;
-    //imageUp: bruceImgUp;
-    //imageDown: bruceImgDowm;
+    direction: DIRECTION.LEFT,
 
-    changeDirection: () => {},
+    changeDirection: (direction) => {
+        this.direction = direction;
+    },
     
     sufferDamage: () => {
-        health--
+        this.health--
     },
 
     crashWith: () => {
         //para choque con enemigos
     }
 }
-
-
-
-// class MasterFighter extends Fighter {
-//     constructor (_direction, _health) {
-//         super(_direction)
-//         this.health = _health;
-//         this.image = bruceImgLeft;
-//         // this.imageLeft = bruceImgLeft;
-//         // this.imageRight = bruceImgRight;
-//         // this.imageUp = bruceImgUp;
-//         // this.imageDown = bruceImgDowm;
-//     }
-    
-//     changeDirection() {}
-    
-//     sufferDamage() {
-//         this.health--;
-//     };
-
-//     crashWith() {
-//         //para choque con enemigos
-//     }  
-// }
